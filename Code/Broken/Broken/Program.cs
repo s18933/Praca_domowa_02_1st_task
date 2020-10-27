ef1d6d101422570b33588c670a8850a93ff8f3a0
Віка Скороход <s18933@pjwstk.edu.pl>
2020-10-27 15:56:55 +0100

    added readme

---
+++ Code/Broken/Broken/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Broken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Skeleton skeleton = new Skeleton();
+
+            skeleton.Name = "Rozitta";
+            skeleton.Age = 60;
+            skeleton.Bones = "Two hundred and seven";
+
+            skeleton.IsAHuman();
+        }
+    }
+}
---
+++ Code/Broken/Broken/Skeleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Broken
+{
+    class Skeleton
+    {
+        public string Name { get; set; }
+        public string Bones { get; set; }
+        public int Age { get; set; }
+
+        public void IsAHuman() {
+            if (Bones < 207 || Bones > 207) {
+                Console.WriteLine("This is not a human");
+            }
+            else
+            {
+                Console.WriteLine("This is a human");
+            }
+        }
+
+    }
+}
